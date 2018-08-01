b609660842371a01dec24b6bc19ac13838681efc
jaimeabbariao <jaimeabbariao@gmail.com>
2018-07-31 20:58:34 -0400

    Finished redux-form course

---
+++ awesome-form/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import RegisterForm from './components/RegisterForm';
+import { SubmissionError } from 'redux-form';
+
+class RegisterFormContainer extends Component {
+    submit = values => {
+        window.alert(JSON.stringify(values, null, 4));
+    }
+
+    getInitialValues() {
+        return {
+            preference: 'spaces',
+            newsletter: true
+        }
+    }
+
+    render() {
+        return <RegisterForm onSubmit={this.submit}
+            initialValues={this.getInitialValues()}
+        />
+    }
+}
+
+export default RegisterFormContainer;
+
